7639a0e58e2962018c506438675fffaf8e2e1473
Keyhanrezvan <Keyhanrezvan@gmail.com>
2023-04-13 18:04:56 -0500

    updatedmigrations

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -7,6 +7,7 @@ exports.up = function (knex) {
     table.string("people");
     table.date("reservation_date");
     table.time("reservation_time");
+    table.string("status").defaultTo("booked").notNullable()
     table.timestamps(true, true);
   });
 };
